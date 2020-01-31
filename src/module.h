a583e596e18ce7df75c5bb5a837cf8c0402b1fd3
altffour <56314286+realaltffour@users.noreply.github.com>
2020-01-31 14:23:30 +0300

    Update module to accomodate for functions

--- src/module.h
+++ src/module.h
@@ -5,11 +5,20 @@
 #include <functional>
 #include <map>
 
+#define MODFN(name, code) \
+    void name(std::string arguments) {code}
+#define MODDISPATCHFN(name, code) \
+    void name(std::string cmd, std::string arguments) {code}
+
 using ModuleFn = std::function<void(std::string arguments)>;
+using ModuleFnDispatcher = std::function<void(std::string cmd,
+        std::string arguments)>;
+
 struct Module {
     std::string _name;
     std::string _desc;
     std::map<std::string, ModuleFn> _funcs;
+    ModuleFnDispatcher _dispatcher;
 };
 
 #endif
